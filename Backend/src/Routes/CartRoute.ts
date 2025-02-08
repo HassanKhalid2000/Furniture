fb8e9f5047127df8c92b873c4a01cb37d7772690
Hassan Khalid <hsnk71568@gmail.com>
2025-02-08 18:56:27 +0300

    Added Add Furniture to cart Service and Endpoint

--- Backend/src/Routes/CartRoute.ts
+++ Backend/src/Routes/CartRoute.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { GetActiveCartForUser } from "../Services/CartService";
+import { AddItemToCart, GetActiveCartForUser } from "../Services/CartService";
 import ValidateJWT, { ExtendRequest } from "../Middleware/ValidateJWT";
 const router = express.Router();
 
@@ -9,4 +9,12 @@ router.get("/", ValidateJWT, async (req: ExtendRequest, res) => {
   res.status(200).send(cart);
 });
 
+//* Add Item To Cart
+router.post('/items',ValidateJWT,async(req:ExtendRequest,res)=>{
+  const userId = req.user._id;
+  const {quantity,productId} =req.body
+  const{data,statusCode}=await AddItemToCart({quantity,userId,productId})
+  res.status(statusCode).send(data)
+
+}) 
 export default router;
--- Backend/src/Services/CartService.ts
+++ Backend/src/Services/CartService.ts
@@ -1,4 +1,5 @@
 import { CartModel } from "../Model/Cart";
+import { ProductModel } from "../Model/Product";
 
 interface IcreateActiveCartForUser {
   userId: string;
@@ -6,7 +7,7 @@ interface IcreateActiveCartForUser {
 const createActiveCartForUser = async ({
   userId,
 }: IcreateActiveCartForUser) => {
-  const cart = await CartModel.create({ userId , totalAmount:0 });
+  const cart = await CartModel.create({ userId, totalAmount: 0 });
 
   await cart.save();
   return cart;
@@ -22,4 +23,34 @@ export const GetActiveCartForUser = async ({
     cart = await createActiveCartForUser({ userId });
   }
   return cart;
+};
+
+//* AddItemToCart
+interface IAddItemToCart {
+  userId: string;
+  productId: any;
+  quantity: number;
+}
+export const AddItemToCart = async ({
+  userId,
+  productId,
+  quantity,
+}: IAddItemToCart) => {
+  const cart = await GetActiveCartForUser({ userId });
+  const existsInCart = cart.items.find((p) => p.product.toString() === productId);
+  if (existsInCart) {
+    return { data: "Item already exits in cart", statusCode: 400 };
+  }
+  const product = await ProductModel.findById(productId);
+  if (!product) {
+    return { data: "product not found", statusCode: 400 };
+  }
+  if(product.stock<quantity){
+    return{data:"low item for stock",statusCode:400}
+  }
+  cart.items.push({ product: productId, quantity, unitPrice: product.price });
+  // update total amount
+  cart.totalAmount+=product.price*quantity
+  const updatedCart = await cart.save();
+  return { data: updatedCart, statusCode: 200 };
 };
