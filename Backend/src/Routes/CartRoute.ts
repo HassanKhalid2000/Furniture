97664ee9778833486ba43155cb20c621b935ce10
Hassan Khalid <hsnk71568@gmail.com>
2025-02-13 00:13:33 +0300

    added clear cart &update item in cart

--- Backend/src/Routes/CartRoute.ts
+++ Backend/src/Routes/CartRoute.ts
@@ -1,5 +1,11 @@
 import express from "express";
-import { AddItemToCart, GetActiveCartForUser } from "../Services/CartService";
+import {
+  AddItemToCart,
+  ClearCart,
+  DeleteItemFromCart,
+  GetActiveCartForUser,
+  UpdateItemInCart,
+} from "../Services/CartService";
 import ValidateJWT, { ExtendRequest } from "../Middleware/ValidateJWT";
 const router = express.Router();
 
@@ -10,11 +16,47 @@ router.get("/", ValidateJWT, async (req: ExtendRequest, res) => {
 });
 
 //* Add Item To Cart
-router.post('/items',ValidateJWT,async(req:ExtendRequest,res)=>{
+router.post("/items", ValidateJWT, async (req: ExtendRequest, res) => {
   const userId = req.user._id;
-  const {quantity,productId} =req.body
-  const{data,statusCode}=await AddItemToCart({quantity,userId,productId})
-  res.status(statusCode).send(data)
+  const { quantity, productId } = req.body;
+  const { data, statusCode } = await AddItemToCart({
+    quantity,
+    userId,
+    productId,
+  });
+  res.status(statusCode).send(data);
+});
 
+//* Update Item In Cart
+router.put("/items", ValidateJWT, async (req: ExtendRequest, res) => {
+  const userId = req.user._id;
+  const { quantity, productId } = req.body;
+  const { data, statusCode } = await UpdateItemInCart({
+    userId,
+    quantity,
+    productId,
+  });
+  res.status(statusCode).send(data);
+});
+
+//* Delete Item From Cart
+router.delete(
+  "/items/:productId",
+  ValidateJWT,
+  async (req: ExtendRequest, res) => {
+    const userId = req.user._id;
+    const { productId } = req.params;
+    const { data, statusCode } = await DeleteItemFromCart({
+      userId,
+      productId,
+    });
+    res.status(statusCode).send(data);
+  }
+);
+//* Clear Cart
+router.delete('/',ValidateJWT,async(req:ExtendRequest,res)=>{
+  const userId=req.user._id;
+  const {data,statusCode}= await ClearCart({userId})
+  res.status(statusCode).send(data)
 }) 
 export default router;
--- Backend/src/Services/CartService.ts
+++ Backend/src/Services/CartService.ts
@@ -1,6 +1,7 @@
 import { CartModel } from "../Model/Cart";
 import { ProductModel } from "../Model/Product";
 
+//* Create Active Cart For User
 interface IcreateActiveCartForUser {
   userId: string;
 }
@@ -12,6 +13,7 @@ const createActiveCartForUser = async ({
   await cart.save();
   return cart;
 };
+//* Get Active Cart For User
 interface IGetActiveCartForUser {
   userId: string;
 }
@@ -37,7 +39,9 @@ export const AddItemToCart = async ({
   quantity,
 }: IAddItemToCart) => {
   const cart = await GetActiveCartForUser({ userId });
-  const existsInCart = cart.items.find((p) => p.product.toString() === productId);
+  const existsInCart = cart.items.find(
+    (p) => p.product.toString() === productId
+  );
   if (existsInCart) {
     return { data: "Item already exits in cart", statusCode: 400 };
   }
@@ -45,12 +49,95 @@ export const AddItemToCart = async ({
   if (!product) {
     return { data: "product not found", statusCode: 400 };
   }
-  if(product.stock<quantity){
-    return{data:"low item for stock",statusCode:400}
+  if (product.stock < quantity) {
+    return { data: "low item for stock", statusCode: 400 };
   }
   cart.items.push({ product: productId, quantity, unitPrice: product.price });
   // update total amount
-  cart.totalAmount+=product.price*quantity
+  cart.totalAmount += product.price * quantity;
+  const updatedCart = await cart.save();
+  return { data: updatedCart, statusCode: 200 };
+};
+//*  Update Item In Cart
+interface IUpdateItemInCart {
+  userId: string;
+  productId: any;
+  quantity: number;
+}
+export const UpdateItemInCart = async ({
+  userId,
+  productId,
+  quantity,
+}: IUpdateItemInCart) => {
+  const cart = await GetActiveCartForUser({ userId });
+  const exixstInCart = cart.items.find(
+    (p) => p.product.toString() === productId
+  );
+  if (!exixstInCart) {
+    return { data: "Item Does Not exisits In Cart", statusCode: 400 };
+  }
+  const product = await ProductModel.findById(productId);
+  if (!product) {
+    return { data: "Product Not Found", statusCode: 400 };
+  }
+  if (product.stock < quantity) {
+    return { data: "Low stock for Item", statusCode: 400 };
+  }
+
+  const otherCartItems = cart.items.filter(
+    (p) => p.product.toString() !== productId
+  );
+  let total = otherCartItems.reduce((sum, product) => {
+    sum += product.unitPrice * product.quantity;
+    return sum;
+  }, 0);
+  exixstInCart.quantity = quantity;
+  total += exixstInCart.quantity * exixstInCart.unitPrice;
+  cart.totalAmount = total;
+  const updatedCart = await cart.save();
+  return { data: updatedCart, statusCode: 200 };
+};
+
+//* Delete Item From Cart
+interface IDeleteItemFromCart {
+  userId: string;
+  productId: any;
+}
+export const DeleteItemFromCart = async ({
+  userId,
+  productId,
+}: IDeleteItemFromCart) => {
+  const cart = await GetActiveCartForUser({ userId });
+  const exisitInCart = cart.items.find(
+    (p) => p.product.toString() === productId
+  );
+  if (!exisitInCart) {
+    return { data: "Item does not exisits in cart", statusCode: 400 };
+  }
+  const product = await ProductModel.findById(productId);
+  if (!product) {
+    return { data: "product not found", statusCode: 400 };
+  }
+  const otherItemsInCart = cart.items.filter(
+    (p) => p.product.toString() !== productId
+  );
+  const total = otherItemsInCart.reduce((sum, product) => {
+    sum += product.quantity * product.unitPrice;
+    return sum;
+  }, 0);
+  cart.totalAmount = total;
+  cart.items = otherItemsInCart;
+  const updatedCart = await cart.save();
+  return { data: updatedCart, statusCode: 200 };
+};
+//* Clear Cart
+interface IClearCart {
+  userId: string;
+}
+export const ClearCart = async ({ userId }: IClearCart) => {
+  const cart = await GetActiveCartForUser({ userId });
+  cart.items = [];
+  cart.totalAmount = 0;
   const updatedCart = await cart.save();
   return { data: updatedCart, statusCode: 200 };
 };
